581a6533be61a42a6d025116399fd5f47d82492f
DekaGie <jakub.krawczyk@proemion.com>
2020-12-24 10:18:33 +0100

    Fake checks.

--- app.js
+++ app.js
@@ -1,150 +1,93 @@
 'use strict';
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-// Imports dependencies and set up http server
-const 
-  request = require('request'),
-  express = require('express'),
-  body_parser = require('body-parser'),
-  app = express().use(body_parser.json()); // creates express http server
 
-// Sets server port and logs message on success
+const request = require('request');
+const express = require('express');
+const body_parser = require('body-parser');
+
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const VERIFY_TOKEN = "secretbzz";
+const SN_FORMAT_VERIFIER = new RegExp('^[0-9]{6,16}$');
+
+const app = express().use(body_parser.json());
+
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-// Accepts POST requests at /webhook endpoint
 app.post('/webhook', (req, res) => {  
 
-  // Parse the request body from the POST
   let body = req.body;
 
-  // Check the webhook event is from a Page subscription
-  if (body.object === 'page') {
+  console.log('POST:');
+  console.log(body);
 
-    body.entry.forEach(function(entry) {
-
-      // Gets the body of the webhook event
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-
-
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id;
-      console.log('Sender ID: ' + sender_psid);
-
-      // Check if the event is a message or postback and
-      // pass the event to the appropriate handler function
-      if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event.message);        
-      } else if (webhook_event.postback) {
-        
-        handlePostback(sender_psid, webhook_event.postback);
-      }
-      
-    });
-    // Return a '200 OK' response to all events
-    res.status(200).send('EVENT_RECEIVED');
-
-  } else {
-    // Return a '404 Not Found' if event is not from a page subscription
+  if (body.object != 'page') {
     res.sendStatus(404);
+    return;
   }
 
+  body.entry.forEach(function(entry) {
+
+    let webhook_event = entry.messaging[0];
+
+    if (!webhook_event.message || !webhook_event.message.text) {
+      return;
+    }
+
+    let sender_psid = webhook_event.sender.id;
+    let input = webhook_event.message.text;
+
+    let response;
+    if (!SN_FORMAT_VERIFIER.test(input)) {
+      response = 'Dzień dobry!\nProszę, podaj mi swój numer karty Beauty ZAZERO.';
+    } else if (input == '113192399') {
+      response = 'Dziękuję!\nTwoja karta jest ważna do 17.02.2021 i obejmuje usługi:\n - Stylizacja Brwi';
+    } else if (input == '113329308') {
+      response = 'Dziękuję!\nTwoja karta jest ważna do 31.12.2021 i obejmuje usługi:\n - Paznokcie\n - Stylizacja Rzęs';
+    } else if (input == '114945246') {
+      response = 'Niestety, Twoja karta straciła ważność 30.11.2020.';
+    } else if (input == '114990607') {
+      response = 'Niestety, Twoja karta straciła ważność 31.08.2020.';
+    } else if (input == '138482385') {
+      response = 'Dziękuję!\nTwoja karta jest ważna do 28.06.2021 i obejmuje usługi:\n - Stylizacja Brwi\n - Stylizacja Rzęs';
+    } else {
+      response = 'Niestety, to nie jest poprawny numer karty. Upewnij się, że przepisałeś wszystkie cyfry znajdujące się pod kodem kreskowym.';
+    }
+
+    callSendAPI(sender_psid, {'text': response});
+  });
+
+  res.status(200).send('EVENT_RECEIVED');
 });
 
-// Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
-  
-  /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = "secretbzz";
-  
-  // Parse params from the webhook verification request
+
+  console.log('GET:');
+  console.log(req.query);
+
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
     
-  // Check if a token and mode were sent
-  if (mode && token) {
-  
-    // Check the mode and token sent are correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
-      // Respond with 200 OK and challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
-    }
+  if (!mode || !token) {
+    res.sendStatus(400);
+    return;
   }
+  
+  if (mode != 'subscribe' || token != VERIFY_TOKEN) {
+    res.sendStatus(403);
+    return;
+  }
+
+  res.status(200).send(challenge);
 });
 
-// Accepts GET requests at the /webhook endpoint
 app.get('/hello', (req, res) => {
+  console.log('HELLO:');
+  console.log(req);
   res.status(200).send("world");
 });
 
-function handleMessage(sender_psid, received_message) {
-  let response;
-  
-  // Checks if the message contains text
-  if (received_message.text) {    
-    // Create the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
-    response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-    }
-  } else if (received_message.attachments) {
-    // Get the URL of the message attachment
-    let attachment_url = received_message.attachments[0].payload.url;
-    response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [{
-            "title": "Is this the right picture?",
-            "subtitle": "Tap a button to answer.",
-            "image_url": attachment_url,
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Yes!",
-                "payload": "yes",
-              },
-              {
-                "type": "postback",
-                "title": "No!",
-                "payload": "no",
-              }
-            ],
-          }]
-        }
-      }
-    }
-  } 
-  
-  // Send the response message
-  callSendAPI(sender_psid, response);    
-}
-
-function handlePostback(sender_psid, received_postback) {
-  console.log('ok')
-   let response;
-  // Get the payload for the postback
-  let payload = received_postback.payload;
-
-  // Set the response based on the postback payload
-  if (payload === 'yes') {
-    response = { "text": "Thanks!" }
-  } else if (payload === 'no') {
-    response = { "text": "Oops, try sending another image." }
-  }
-  // Send the message to acknowledge the postback
-  callSendAPI(sender_psid, response);
-}
-
 function callSendAPI(sender_psid, response) {
-  // Construct the message body
+
   let request_body = {
     "recipient": {
       "id": sender_psid
@@ -152,7 +95,9 @@ function callSendAPI(sender_psid, response) {
     "message": response
   }
 
-  // Send the HTTP request to the Messenger Platform
+  console.log('SENDING...');
+  console.log(response);
+
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
     "qs": { "access_token": PAGE_ACCESS_TOKEN },
@@ -160,9 +105,9 @@ function callSendAPI(sender_psid, response) {
     "json": request_body
   }, (err, res, body) => {
     if (!err) {
-      console.log('message sent!')
+      console.log('...SUCCESS')
     } else {
-      console.error("Unable to send message:" + err);
+      console.error('...ERROR: ' + err);
     }
   }); 
 }
